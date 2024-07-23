94eb92996a9e35a2bbc77068e2b470fa5d2672a3
Ubaid Ur Rehman <119524246+UBAID0315@users.noreply.github.com>
2024-07-23 20:49:52 +0500

    Add files via upload

---
+++ MyComponents/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Spinner = () => {
+    return (
+        <div>
+            <div className="text-center align-self-center mt-5" style={{color:'#0078D4'}}>
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+                <h6 className='mt-2'style={{fontFamily:"revert-layer"}}>Please Wait! while we choose the best for you</h6>
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
